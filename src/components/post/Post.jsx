6ee243bfc63fcff30dcf75d8a253013f606e8410
Nisfuuu <nisfu4553@gmail.com>
2023-01-19 20:33:34 +0700

    social media build in react.js

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,35 @@
+import { MoreVert } from "@mui/icons-material";
+import "./post.css";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <span className="postUsername">Safak Kacoglu</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey! its my post:)</span>
+          <img className="postImg" src="assets/post/1.jpeg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/heart.png" alt="" />
+            <span className="postLikeConter">32 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,101 @@
+import "./sidebar.css";
+import { HelpOutline, RssFeed, School, WorkOutline, Event, PlayCircleFilledOutlined, Group, Chat } from "@mui/icons-material";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListitemText">feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListitemText">Chats</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircleFilledOutlined className="sidebarIcon" />
+            <span className="sidebarListitemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarListitemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListitemText">Bookmark</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarListitemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <WorkOutline className="sidebarIcon" />
+            <span className="sidebarListitemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListitemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarListitemText">Courses</span>
+          </li>
+        </ul>
+        <button className="sidebarBotton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendimg" src="./assets/person/2.jpeg" alt="" />
+            <span className="sidebarFrindName">Jane Doe</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
